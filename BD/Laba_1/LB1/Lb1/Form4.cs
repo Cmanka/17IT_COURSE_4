1a2677843c6f848208237e8e41aaa7dbba3080b3
Alexander Trubkin <offatrubkin@gmail.com>
2020-09-08 18:56:40 +0300

    Was done Laba_1 BD

---
+++ BD/Laba_1/LB1/Lb1/Form4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lb1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1;
+            double num2;
+            double num3;
+            double result;
+
+            if (double.TryParse(textBox1.Text, out num1) && 
+                double.TryParse(textBox2.Text, out num2) &&
+                double.TryParse(textBox3.Text, out num3))
+            {
+                if (num1 > 0 && num2 > 0 && num3 > 0)
+                {
+                    result = num1 * num2 * num3;
+                    textBox4.Text = result.ToString();
+                }
+                else
+                    textBox4.Text = "Данные введены неверно!";
+            }
+            else
+                textBox4.Text = "Данные введены неверно!";
+        }
+    }
+}
